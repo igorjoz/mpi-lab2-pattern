64ec64bbc00fd753633871c817f936262464ed0d
Igor Józefowicz <igorjozefowicz@gmail.com>
2026-03-30 00:19:43 +0200

    add lab2 basic files

---
+++ mpi.c
@@ -0,0 +1,46 @@
+#include "utility.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <mpi.h>
+#include "numgen.c"
+
+int main(int argc,char **argv) {
+
+  Args ins__args;
+  parseArgs(&ins__args, &argc, argv);
+
+  //program input argument
+  long inputArgument = ins__args.arg; 
+
+  struct timeval ins__tstart, ins__tstop;
+
+  int myrank,nproc;
+  unsigned long int *numbers;
+
+  MPI_Init(&argc,&argv);
+
+  // obtain my rank
+  MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+  // and the number of processes
+  MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+
+  if(!myrank){
+      	gettimeofday(&ins__tstart, NULL);
+	numbers = (unsigned long int*)malloc(inputArgument * sizeof(unsigned long int));
+  	numgen(inputArgument, numbers);
+  }
+
+  // run your computations here (including MPI communication)
+
+
+  // synchronize/finalize your computations
+
+  if (!myrank) {
+    gettimeofday(&ins__tstop, NULL);
+    ins__printtime(&ins__tstart, &ins__tstop, ins__args.marker);
+  }
+  
+  MPI_Finalize();
+
+}
